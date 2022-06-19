834a0e716023864aa30fa173d2e5df8fabf30793
drlux <sorrentino.luca@outlook.com>
2022-06-20 00:58:55 +0200

    update dockerfiles

--- bots/heikinAshiChaser/run_bot.sh
+++ bots/heikinAshiChaser/run_bot.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-docker build . -t heikinashichaser
-docker run heikinashichaser
-#echo "prova" > test.txt
-#python src/heikinashichaser/main.py 
+PLATFORM="arm"
+
+if ! [ "$(ls -A ./externalPackage/)" ] 
+then 
+    if ! [ "$(ls -A ../../framework/dist/)" ] 
+    then 
+        cd ../../framework/
+        /bin/bash create_wheel.sh
+        cd ../bots/heikinAshiChaser/
+    fi
+
+    cp ../../framework/dist/*.whl ./externalPackage/ 
+fi
+    
+
+if [[ $PLATFORM == "pc" ]]
+then
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes 
+    docker build . -t heikinashichaser --build-arg platform=arm  
+    docker run --rm -t -v $PWD:/heikinashichaser  heikinashichaser
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p no 
+else 
+    docker build . -t heikinashichaser --build-arg platform=pc  
+    docker run --rm -t -it -v $PWD:/heikinashichaser  heikinashichaser
+fi
